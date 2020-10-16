87d039c954bd1ed1d2e8a76f0e1a3bad9ce61c64
Vitaliy Pavlov <vitaliypavlov33@gmail.com>
2020-10-16 15:27:48 +0300

    realized list module

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: JSON.parse( localStorage.getItem('tasks') || '[]' )
   },
   mutations: {
+    CreateNewTask(state, task) {
+      state.tasks.push(task)
+
+      localStorage.setItem('tasks', JSON.stringify(state.tasks))
+    }
   },
   actions: {
+    CreateNewTask({commit}, task) {
+      commit('CreateNewTask', task)
+    }
   },
-  modules: {
+  getters: {
+    tasks: s => s.tasks
   }
 })
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,9 +3,9 @@
     <div class="col s6 offset-s3">
       <h1>Create task</h1>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
-          <input id="first_name" placeholder="Tilte" type="text" class="validate" required>
+          <input id="first_name" placeholder="Tilte" v-model="title" type="text" class="validate" required>
           <label for="title"></label>
           <span class="helper-text" data-error="Title is required"></span>
         </div>
@@ -17,6 +17,9 @@
           <label for="description">Textarea</label>
           <span class="character-counter" style="float: right; font-size: 12px;">{{description.length}}/2048</span>
         </div>
+
+        <input type="text" ref="datepicker">
+        <button class="btn" type="submit">Create task</button>
       </form>
     </div>
   </div>
@@ -28,12 +31,43 @@ export default {
   components: {},
   data: () => ({
       description: '',
-      title: ''
+      title: '',
+      chips: null,
+      date: null
   }),
   mounted() {
-    M.Chips.init(this.$refs.chips, {
+    this. chips = M.Chips.init(this.$refs.chips, {
       placeholder: 'Task tags'
-    })
+    });
+    this.date = M.Datepicker.init(this.$refs.datepicker, {
+      format: 'dd.mm.yyyy',
+      defaultDate: new Date(),
+      setDefaultDate: true
+    });
+  },
+  methods: {
+    submitHandler() {
+      const task = {
+        title: this.title,
+        description: this.description,
+        id: Date.now(),
+        status: 'active',
+        tags: this.chips.chipsData,
+        date: this.date.date
+      }
+
+      this.$store.dispatch('CreateNewTask', task)
+      this.$router.push('/list')
+    }
+  },
+  destroyed() {
+    if(this.date && this.date.destroy) {
+      this.date.destroy()
+    }
+
+    if(this.chips && this.chips.destroy) {
+      this.chips.destroy()
+    }
   }
 }
 </script>
